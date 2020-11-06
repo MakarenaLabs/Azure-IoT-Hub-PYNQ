8c060646f1200735a6c93a230fa8476d9c592926
Enrico Giordano <enricogiordano1992@gmail.com>
2020-11-06 17:42:37 +0100

    Update setup_azure_iot_hub.sh

--- setup_azure_iot_hub.sh
+++ setup_azure_iot_hub.sh
@@ -92,8 +92,8 @@ JSON_STRING=$( jq -n \
                   --arg gpio "$_gpio" \
                   '{bitstream: $bn, 
                     addresses:{
-                        uart: $_uart,
-                        gpio: $_gpio
+                        uart: $uart,
+                        gpio: $gpio
                     }}' )
 
 echo $JSON_STRING > config.json
